a2bfe77a88f3d5b5e694566a46858d8a8c9726e1
biunovich <biunovich@gmail.com>
2015-03-14 19:31:41 +0600

    GAUSS
    GAUSS

--- test/VYCH2.3/VYCH2.3/VYCH2.3.c
+++ test/VYCH2.3/VYCH2.3/VYCH2.3.c
@@ -60,6 +60,29 @@ double eps_integral_simp(double eps)
 	printf("ITERATION: %d\n", i);
 	return rezult2;
 }
+double integral_gauss(int n)
+{
+	int i;
+	double step = (double)1 / n;
+	double point1 = step / 2 - step*sqrt(3 / 5) / 2, point2 = step / 2, point3 = step / 2 + step*sqrt(3 / 5) / 2,rezult = 0;
+	for (i = 0; i < n; i++)
+		rezult += (5 * funk(point1 + i*step) + 8 * funk(point2 + i*step) + 5 * funk(point3 + i*step))*step / 18;
+	return rezult;
+}
+double eps_integral_gauss(double eps)
+{
+	int i=0;
+	double step = 1, rezult1 = 0, rezult2 = 1000;
+	while (fabs(rezult1 - rezult2) > eps)
+	{
+		i++;
+		rezult1 = integral_gauss(step);
+		step *= 2;
+		rezult2 = integral_gauss(step);
+	}
+	printf("ITERATION: %d\n", i);
+	return rezult2;
+}
 int main()
 {
 	int n[] = { 20, 50, 100 }, i;
@@ -79,13 +102,16 @@ int main()
 		znach = integral_simp(n[i]);
 		printf("SETKA: %d  ANSWER: %lf\n", n[i], znach);
 	}
+	printf("FORMULA GAUSSA 3\n");
+	for (i = 0; i < 3; i++)
+	{
+		znach = integral_gauss(n[i]);
+		printf("SETKA: %d  ANSWER: %lf\n", n[i], znach);
+	}
 	printf("\nENTER EPS: ");
 	scanf("%lf", &eps);
-	printf("FORMULA TRAPETSYI\n");
-	znach = eps_integral_trap(eps);
-	printf("ANSWER: %0.8lf\n", znach);
-	printf("FORMULA SIMPSONA\n");
-	znach = eps_integral_simp(eps);
-	printf("ANSWER: %0.8lf\n", znach);
+	printf("FORMULA TRAPETSYI\nANSWER: %0.8lf\n", eps_integral_trap(eps));
+	printf("FORMULA SIMPSONA\nANSWER: %0.8lf\n", eps_integral_simp(eps));
+	printf("FORMULA GAUSSA 3\nANSWER: %0.8lf\n", eps_integral_gauss(eps));
 	return 0;
 }
