6c5957ff590f9b83d31e40a0d9de1d4ac3905b07
biunovich <biunovich@gmail.com>
2015-05-12 21:26:06 +0600

    COOL
    GUI RSA

---
+++ test/GUI/GUI/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string RadioButtonText;
+        string RSAPubKey;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EncryptDecrypt_Button(object sender, RoutedEventArgs e)
+        {
+            if (RadioButtonText=="Encrypt")
+            {
+                EncryptedData.Text = RSACrypt.Encrypt_Text(Encoding.Unicode.GetBytes(DataToEncrypt.Text));
+            }
+            else if (RadioButtonText=="Decrypt")
+            {
+                DecryptedData.Text = RSACrypt.Decrypt_Text(Convert.FromBase64String(EncryptedData.Text));
+            }
+        }
+        private void CreateKey_Button(object sender, RoutedEventArgs e)
+        {
+            RSACrypt.Create_Key();
+        }
+        private void Encrypt(object sender, RoutedEventArgs e)
+        {
+            var button = sender as RadioButton;
+            RadioButtonText = button.Content.ToString();
+        }
+
+        private void ImportPubKey_Button(object sender, RoutedEventArgs e)
+        {
+            RSAPubKey = ImportPubKey.Text;
+        }
+
+        private void EncryptWithPubKey_Button(object sender, RoutedEventArgs e)
+        {
+            EncryptedData.Text = RSACrypt.EncryptWithPubKey(RSAPubKey, Encoding.Unicode.GetBytes(DataToEncrypt.Text));
+        }
+
+        private void DataToEncrypt_Box(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void DecryptedData_Box(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void EncryptedData_Box(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ test/GUI/GUI/RSACrypt.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using System.Windows;
+
+namespace GUI
+{
+    class RSACrypt
+    {
+        public static void Create_Key()
+        {
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                File.WriteAllText("RSAKEY.xml", RSA.ToXmlString(true));
+                File.WriteAllText("RSAPUBKEY.xml", RSA.ToXmlString(false));
+            }
+        }
+        public static string Encrypt_Text(byte[] DataToEncrypt)
+        {
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                string rsaKey = File.ReadAllText("RSAKEY.xml");
+                RSA.FromXmlString(rsaKey);
+                return RSAEncrypt(DataToEncrypt, RSA.ExportParameters(false), false);
+            }
+        }
+        private static string RSAEncrypt(byte[] dataToEncrypt, RSAParameters rSAParameters, bool p)
+        {
+            byte[] encryptedData;
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                RSA.ImportParameters(rSAParameters);
+                encryptedData = RSA.Encrypt(dataToEncrypt, p);
+                return Convert.ToBase64String(encryptedData);
+            }
+        }
+
+        public static string Decrypt_Text(byte[] encryptedData)
+        {
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                string rsaKey = File.ReadAllText("RSAKEY.xml");
+                RSA.FromXmlString(rsaKey);
+                return RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
+            }
+        }
+        private static string RSADecrypt(byte[] encryptedData, RSAParameters rSAParameters, bool p)
+        {
+            byte[] decryptedData;
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                RSA.ImportParameters(rSAParameters);
+                decryptedData = RSA.Decrypt(encryptedData, p);
+                return Encoding.Unicode.GetString(decryptedData);
+            }
+        }
+        public static string EncryptWithPubKey(string pathPubKey,byte[] DataToEncrypt)
+        {
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                string PubKey = File.ReadAllText(pathPubKey);
+                RSA.FromXmlString(PubKey);
+                //MessageBox.Show(RSA.ToXmlString(false));
+                return RSAEncrypt(DataToEncrypt, RSA.ExportParameters(false), false);
+            }
+        }
+    }
+}
